c294184468a24e39f73cec55087d9d8891c6a1b4
albert.fang <albert.fang@51job.com>
2020-05-27 13:37:51 +0800

    test git update

--- src/main/java/com/xm/jy/job_51/mianTest/Test_51.java
+++ src/main/java/com/xm/jy/job_51/mianTest/Test_51.java
@@ -7,14 +7,6 @@ package com.xm.jy.job_51.mianTest;
  */
 public class Test_51 {
     public static void main(String[] args) {
-       String pidAndReportIds = "t_551~65, t_398~1458, t_570~1916";
-       String pidAndApids = "t_398:10271349,10271311,10248721--t_551:10248721,10248531,10248279,10248272--t_570:10248721";
-        String[] report=pidAndReportIds.split(",");
-        for (int i = 0; i < report.length; i++) {
-            String pidAndReportId = report[i]+":";
-            String product = report[i].split("~")[0]+":";
-            pidAndApids = pidAndApids.replaceFirst(product, pidAndReportId);
-        }
-        System.out.println(pidAndApids);
+        System.out.println(1);
     }
 }
