d95968769f17f77453e6d7a29ae86e8efd840b2f
albert.fang <albert.fang@51job.com>
2020-05-28 17:38:48 +0800

    @Configuration、@Component

--- src/main/java/com/xm/jy/test/ioc/config/GoodStudyConfig.java
+++ src/main/java/com/xm/jy/test/ioc/config/GoodStudyConfig.java
@@ -13,11 +13,7 @@ public class GoodStudyConfig {
 
     @Bean(name = "encourageSelf")
     public static String encourageSelf(){
-        return "\033[31;0m"+"从今天开始，有计划的学习，mark 2020/5/28 17:19......";
+        return "\033[31;0m"+"从今天开始，有计划的学习......";
     }
 
-    @Bean(name = "believeMe")
-    public static String believeMe(){
-        return "相信我，这次绝非是口头说说而已......"+"\033[0m";
-    }
 }
