b7c78abe4c98f12ca5841b5e8269eaa08f97d1ba
albert.fang <albert.fang@51job.com>
2020-06-17 14:21:37 +0800

    awt下面的Panel练习； 1:和Frame类似有setBounds、setBackground、setSize、setLocation方法 2:通过frame.add(panel)往frame里添加panel

---
+++ src/main/java/com/xm/jy/job_51/awt/TestPanel.java
@@ -0,0 +1,36 @@
+package com.xm.jy.job_51.awt;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class TestPanel {
+    public static void main(String[] args) {
+        Frame frame = new Frame();
+        Panel panel = new Panel();
+
+        //设置布局(布局：Layout)
+        frame.setLayout(null);
+
+        frame.setBounds(300, 300, 600, 600);
+
+        frame.setBackground(new Color(85,150,68));
+
+        panel.setBounds(50,50,400,400);
+        panel.setBackground(new Color(105,39,0));
+		
+        // panel内嵌在frame里面
+        frame.add(panel);
+        
+        frame.setVisible(true);
+		
+        //解决窗口点击X无法关闭问题
+        //(监听者:Listener) 监听窗口关闭事件：System.exit(0)
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+}
