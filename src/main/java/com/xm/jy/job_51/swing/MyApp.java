517feaf49c68b8f7f41b6560e2c897912fba5c80
albert.fang <albert.fang@51job.com>
2020-06-17 14:16:02 +0800

    awt下面的Frame练习； frame.setBackground给框架设置背景颜色； frame.setSize(width,height)给框架设置宽高 frame.setLocation(x,y)框架右上角和屏幕右上角的衡量 frame.setBounds(x,y,w,h)相当于setLocation、setSize两个方法的结合 frame.setResizable(true/false)是否允许手动给框架拉长或拉高 frame.setVisible(true/false)框架是否可见

---
+++ src/main/java/com/xm/jy/job_51/swing/MyApp.java
@@ -0,0 +1,24 @@
+package com.xm.jy.job_51.swing;
+
+import javax.swing.*;
+
+public class MyApp {
+    MyApp(String[] args) {
+        // Invoked on the event dispatching thread. Do any initialization
+        // here.
+    }
+
+    public void show() {
+        // Show the UI.
+    }
+
+    public static void main(final String[] args) {
+        // Schedule a job for the event-dispatching thread:
+        // creating and showing this application's GUI.
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new MyApp(args).show();
+            }
+        });
+    }
+}
