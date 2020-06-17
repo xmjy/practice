41df69701e675e33ab7820b7237e30a18af467de
albert.fang <albert.fang@51job.com>
2020-06-17 15:09:17 +0800

    awt下面的Layout练习； 流布局：flowLayout练习

---
+++ src/main/java/com/xm/jy/job_51/awt/TestFlowLayout.java
@@ -0,0 +1,29 @@
+package com.xm.jy.job_51.awt;
+
+import java.awt.*;
+
+/**
+ * 布局管理器之流式布局 FlowLayout
+ */
+public class TestFlowLayout {
+    public static void main(String[] args) {
+        Frame frame = new Frame();
+
+        //设置按钮
+        Button button1 = new Button("button1");
+        Button button2 = new Button("button2");
+        Button button3 = new Button("button3");
+
+        //按钮左中右分布
+        frame.setLayout(new FlowLayout(FlowLayout.RIGHT));
+
+        frame.add(button1);
+        frame.add(button2);
+        frame.add(button3);
+
+        frame.setLocation(600,600);
+        frame.setSize(400,400);
+
+        frame.setVisible(true);
+    }
+}
