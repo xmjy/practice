6e460f6efc79b35ec7f63babb822217b53c88afa
albert.fang <albert.fang@51job.com>
2020-09-24 10:57:50 +0800

    Merge branch 'master' of E:\IdeaProjects\practice with conflicts.

--- src/main/java/com/xm/jy/job_51/mianTest/Test_51.java
+++ src/main/java/com/xm/jy/job_51/mianTest/Test_51.java
@@ -1,7 +1,6 @@
 package com.xm.jy.job_51.mianTest;
 
-import java.util.*;
-import java.util.concurrent.atomic.AtomicReference;
+import java.text.ParseException;
 
 /**
  * @author: albert.fang
@@ -9,12 +8,7 @@ import java.util.concurrent.atomic.AtomicReference;
  * @description: 随便测试的内容，随测随删
  */
 public class Test_51 {
-    public static void main(String[] args) {
-        Map<Integer,Integer> map = new HashMap<>();
-        map.put(1,2);
-        if (map.containsKey(1)){
-            map.put(1,map.get(1) + 1);
-        }
-        System.out.println(map.get(1));
+    public static void main(String[] args) throws ParseException, InterruptedException {
+
     }
 }
