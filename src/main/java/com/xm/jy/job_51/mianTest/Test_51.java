e197fff93981478ef7b3398ff08bea4df49517bd
albert.fang <albert.fang@51job.com>
2020-09-24 11:01:04 +0800

    git test

--- src/main/java/com/xm/jy/job_51/mianTest/Test_51.java
+++ src/main/java/com/xm/jy/job_51/mianTest/Test_51.java
@@ -1,16 +1,6 @@
 package com.xm.jy.job_51.mianTest;
 
-import com.xm.jy.job_51.util.PropertiesUtil;
-import jdk.nashorn.internal.runtime.regexp.RegExp;
-
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.*;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * @author: albert.fang
