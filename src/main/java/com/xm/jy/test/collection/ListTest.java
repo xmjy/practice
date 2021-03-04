ece503bb6a73cc2ef1190d354eec5e1e2c51dab9
albert.fang <albert.fang@51job.com>
2021-03-04 16:46:56 +0800

    indexOf、Collections.swap,add(index,element)

--- src/main/java/com/xm/jy/test/collection/ListTest.java
+++ src/main/java/com/xm/jy/test/collection/ListTest.java
@@ -1,34 +1,44 @@
 package com.xm.jy.test.collection;
 
-import java.util.LinkedList;
-import java.util.List;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * @auther 方翔鸣
  * @date 2020/3/7 13:04
  */
 public class ListTest {
-    private List<Integer> sorted = new LinkedList<>();
-    public void addElement(int n) {
-        // TODO
-    }
-    public static void main(String[] args) {
-        LinkedList<Integer> sorted = new LinkedList<>();
-        sorted.add(1);
-        sorted.get(1);
-        for (int i = 0; i < sorted.size()-1; i++) {
-            for (int j = 0; j < sorted.size()-i-j; j++){
-                if (sorted.get(j) > sorted.get(j + 1)) {
-                    int first = sorted.get(j);
-                    int last = sorted.get(j + 1);
-                    int temp = first;
-                    first = last;
-                    last = temp;
-                    sorted.add(first);
-                    sorted.add(last);
-                }
-            }
-        }
 
+    public static void main(String[] args) {
+        ArrayList<User> arrayList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            arrayList.add(new User(i+1,"张"+i,30-i));
+        }
+        // 0 和 2 这两个位置的元素交换
+        Collections.swap(arrayList,0,2);
+
+        // 将index 为 9 的提到首位 add(index,element)
+        User removeElement = arrayList.remove(9);
+        arrayList.add(0,removeElement);
+        arrayList.forEach(System.out::println);
+
+        // 获取具体元素的index,indexOf
+        System.out.println(arrayList.indexOf(arrayList.get(6)));
+
+    }
+}
+
+@Data
+class User{
+    int id;
+    String name;
+    int age;
+
+    public User(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
     }
 }
