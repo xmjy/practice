27379ffc0d16418e89df2c70cd88847c168f800c
albert.fang <albert.fang@51job.com>
2021-06-15 16:35:34 +0800

    delete

---
+++ src/main/java/com/xm/jy/test/mianTest/ArrayTest.java
@@ -0,0 +1,12 @@
+package com.xm.jy.test.mianTest;
+
+/**
+ * @author: albert.fang
+ * @date: 2021/5/26 9:24
+ * @description: 数组测试
+ */
+public class ArrayTest {
+    public static void main(String[] args) {
+
+    }
+}
